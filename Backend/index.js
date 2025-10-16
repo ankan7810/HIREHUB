74f4f63d1b8ef84e0c2db1cb1b4b5f0cc5f904bf
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-10-16 23:38:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import fs from "fs";
 
 dotenv.config({});
 const app = express();
@@ -17,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://hirehub-frontend-9ks8.onrender.com"],
+  origin: ["http://localhost:5173"],
   credentials: true,
 };
 
@@ -32,6 +33,8 @@ app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
+
+
 
 app.listen(PORT, () => {
   connectDB();
